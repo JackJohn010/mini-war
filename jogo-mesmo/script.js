c65e883413cc92c8432d7bdaf86f64ca3ff23216
Johnny <johnnysilva991@gmail.com>
2023-05-02 12:37:02 -0300

    migracao para jogo-mesmo

--- jogo-mesmo/script.js
+++ jogo-mesmo/script.js
@@ -1,20 +1,24 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+//Dimensões do canva
+canvas.width = 800;
+canvas.height = 500;
+
 // Define os tanques e suas posições iniciais
 var tank1 = {
     x: 50,
-    y: canvas.height - 50,
-    width: 50,
-    height: 30,
-    color: "green"
+    y: canvas.height - 480,
+    width: 60,
+    height: 50,
+    color: "darkblue"
 };
 
 var tank2 = {
     x: canvas.width - 100,
-    y: canvas.height - 50,
-    width: 50,
-    height: 30,
+    y: canvas.height - 80,
+    width: 60,
+    height: 50,
     color: "red"
 };
 
@@ -30,17 +34,33 @@ drawTank(tank2);
 // Move os tanques com as teclas do teclado
 document.addEventListener("keydown", function(event) {
     switch(event.keyCode) {
+        
+        //Controle Player 1
         case 37: // Esquerda
-            tank1.x -= 10;
+            tank2.x -= 15;
             break;
         case 39: // Direita
-            tank1.x += 10;
+            tank2.x += 15;
             break;
+        case 38: // Cima
+            tank2.y += -15;
+            break;
+        case 40: // Baixo
+            tank2.y += 15;
+            break;
+
+        //Controle Player 2
         case 65: // A
-            tank2.x -= 10;
+            tank1.x -= 15;
             break;
         case 68: // D
-            tank2.x += 10;
+            tank1.x += 15;
+            break;
+        case 87: // W
+            tank1.y += -15;
+            break;
+        case 83: // S
+            tank1.y += 15;
             break;
     }
 });
@@ -53,25 +73,6 @@ function update() {
 }
 
 setInterval(update, 10);
-
-// Adiciona a lógica para detectar quando um tanque é atingido
-function checkCollisions() {
-    if (intersects(tank1, tank2)) {
-        // Se os tanques se sobrepõem, o jogo acaba e o tanque atingido perde
-        alert("Game over! Tank 1 wins.");
-    }
-}
-
-// Verifica se dois retângulos se sobrepõem
-function intersects(rect1, rect2) {
-    return !(rect1.x + rect1.width < rect2.x || 
-             rect2.x + rect2.width < rect1.x || 
-             rect1.y + rect1.height < rect2.y ||
-             rect2.y + rect2.height < rect1.y);
-}
-
-// Verifica as colisões a cada 10 milissegundos
-setInterval(checkCollisions, 10);
 
 // Define a classe Bullet para representar as balas
 class Bullet {
@@ -105,11 +106,11 @@ document.addEventListener("keydown", function(event) {
     switch(event.keyCode) {
         case 32: // Barra de espaço
             // Cria uma bala na posição do tanque que atirou com uma velocidade para cima
-            bullets.push(new Bullet(tank1.x + tank1.width / 2, tank1.y - 5, {x: 0, y: -10}));
+            bullets.push(new Bullet(tank1.x + tank1.width / 2, tank1.y - 5, {x: 10, y: 1}));
             break;
         case 13: // Enter
             // Cria uma bala na posição do tanque que atirou com uma velocidade para baixo
-            bullets.push(new Bullet(tank2.x + tank2.width / 2, tank2.y + tank2.height + 5, {x: 0, y: 10}));
+            bullets.push(new Bullet(tank2.x + tank2.width / 2, tank2.y + tank2.height + 5, {x: -10, y: -1}));
             break;
     }
 });
@@ -125,61 +126,92 @@ function updateBullets() {
 
 setInterval(updateBullets, 10);
 
-var startButton = document.getElementById("start-button");
-startButton.addEventListener("click", function() {
-    // Coloque aqui a lógica para iniciar o jogo
+
+function checkCollisions() {
+    if (intersects(tank1, tank2)) {
+        // Se os tanques se sobrepõem, o jogo acaba e o tanque atingido perde
+        alert("Game over! Os tanks se sobreporam.");
+    }
     
-});
-
-startButton.addEventListener("click", function() {
-    // Cria os tanques
-    var tank1 = new Tank(50, 50, "blue");
-    var tank2 = new Tank(500, 500, "red");
-
-    // Adiciona os tanques ao canvas
-    tank1.draw();
-    tank2.draw();
-
-    // Adiciona a lógica para mover os tanques
-    document.addEventListener("keydown", function(event) {
-        switch(event.keyCode) {
-            case 65: // A
-                tank1.moveLeft();
-                break;
-            case 68: // D
-                tank1.moveRight();
-                break;
-            case 87: // W
-                tank1.moveUp();
-                break;
-            case 83: // S
-                tank1.moveDown();
-                break;
-            case 37: // Setinha para a esquerda
-                tank2.moveLeft();
-                break;
-            case 39: // Setinha para a direita
-                tank2.moveRight();
-                break;
-            case 38: // Setinha para cima
-                tank2.moveUp();
-                break;
-            case 40: // Setinha para baixo
-                tank2.moveDown();
-                break;
+    // Verifica se o tanque 1 foi atingido
+    projectiles.forEach(function(projectile) {
+        if (intersects(tank1, projectile)) {
+            // Se o tanque 1 foi atingido, o jogo acaba e o tanque 2 vence
+            alert("Game over! Tank 2 wins.");
         }
     });
-
-    // Adiciona a lógica para detectar colisões
-    function checkCollisions() {
-        if (intersects(tank1, tank2)) {
+    
+    // Verifica se o tanque 2 foi atingido
+    projectiles.forEach(function(projectile) {
+        if (intersects(tank2, projectile)) {
+            // Se o tanque 2 foi atingido, o jogo acaba e o tanque 1 vence
             alert("Game over! Tank 1 wins.");
         }
+    });
+}
+
+
+// Verifica se dois retângulos se sobrepõem
+function intersects(rect1, rect2) {
+    return !(rect1.x + rect1.width < rect2.x || 
+             rect2.x + rect2.width < rect1.x || 
+             rect1.y + rect1.height < rect2.y ||
+             rect2.y + rect2.height < rect1.y);
+}
+
+// Verifica as colisões a cada 10 milissegundos
+setInterval(checkCollisions, 10);
+
+function checkCollisions() {
+    if (intersects(tank1, tank2)) {
+        // Se os tanques se sobrepõem, o jogo acaba e o tanque atingido perde
+        alert("Game over! Os tanks se sobreporam.");
+        restartGame();
     }
+    
+    // Verifica se o tanque 1 foi atingido
+    projectiles.forEach(function(projectile) {
+        if (intersects(tank1, projectile)) {
+            // Se o tanque 1 foi atingido, o jogo acaba e o tanque 2 vence
+            alert("Game over! Tank 2 wins.");
+            restartGame();
+        }
+    });
+    
+    // Verifica se o tanque 2 foi atingido
+    projectiles.forEach(function(projectile) {
+        if (intersects(tank2, projectile)) {
+            // Se o tanque 2 foi atingido, o jogo acaba e o tanque 1 vence
+            alert("Game over! Tank 1 wins.");
+            restartGame();
+        }
+    });
+}
 
+function restartGame() {
+    // Reinicia a posição dos tanques
+    tank1.x = 50;
+    tank1.y = canvas.height - 480;
+    tank2.x = canvas.width - 100;
+    tank2.y = canvas.height - 80;
+    
+    // Limpa a lista de balas
+    bullets = [];
+    
+    // Remove o alerta de game over, caso exista
+    var alerts = document.querySelectorAll(".alert");
+    for (var i = 0; i < alerts.length; i++) {
+        alerts[i].remove();
+    }
+    
+    // Reinicia o jogo
+    setInterval(update, 10);
+    setInterval(updateBullets, 10);
     setInterval(checkCollisions, 10);
+}
 
-    // Adiciona a lógica para atirar com os tanques
-    // ...
+// inciar jogo
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", iniciarJogo);
 
-});
+iniciarJogo();
